62016dab6fcf6df7d958f1ae3bc4e73d4ec833a2
Logan <02logan.pennock@gulllakecs.org>
2020-02-05 09:52:58 -0500

    add + and -

--- app/src/main/java/com/example/armada20/MainActivity.java
+++ app/src/main/java/com/example/armada20/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.armada20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+int firstShield = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void shield1plus(View view) {
+    firstShield = firstShield + 1;
+    display(firstShield);
+    }
+    public void shield1minus(View view) {
+        firstShield = firstShield - 1;
+        display(firstShield);
+    }
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
     }
 }
